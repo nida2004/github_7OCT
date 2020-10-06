3499febd778742dfabd3b2a1d67b5a5406220939
nida2004 <nyilmaz2025@gmail.com>
2020-10-07 01:11:29 +0200

    first commiting

---
+++ src/main/java/October7.java
@@ -0,0 +1,7 @@
+public class October7 {
+
+    public static void main(String[] args) {
+        System.out.println("Self training gitHub");
+        System.out.println("New file changed");
+    }
+}
