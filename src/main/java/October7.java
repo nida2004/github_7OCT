a3e89cee376862e42396b5e9baeb6eeccff41cd2
nida2004 <nyilmaz2025@gmail.com>
2020-10-07 22:36:38 +0200

    branch merge

--- src/main/java/October7.java
+++ src/main/java/October7.java
@@ -3,5 +3,10 @@ public class October7 {
     public static void main(String[] args) {
         System.out.println("Self training gitHub");
         System.out.println("New file changed");
+
+
+        //7 october branch merge
+        //testing
+
     }
 }
